718f837d48e1ebfc693f77d4be7d4742184b9664
unknown <bavleydanial@gmail.com>
2023-06-27 03:34:04 +1000

    Setup SFML and created a test window

--- DunnEngine/Source/Application.cpp
+++ DunnEngine/Source/Application.cpp
@@ -16,10 +16,33 @@ namespace DunnEngine {
 	{
 		OnInit();
 
-		while (true)
-		{
-			OnUpdate();
-		}
+        sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setOrigin(shape.getRadius(), shape.getRadius());
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+
+                if (event.type == sf::Event::Resized)
+                {
+                    // update the view to the new size of the window
+                    sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                    window.setView(sf::View(visibleArea));
+                }
+                shape.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+            }
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
 	}
 
 }
